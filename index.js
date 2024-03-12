4b06d1051019644c3fc56b1276216112702a1716
poulpyR2r <guewencarre@gmail.com>
2024-03-12 13:50:28 +0100

    add env

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 
 const ScoreRoute = "./routes/scores.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3005;
+const IP_ADDRESSE = process.env.IP_ADDRESSE || "localhost";
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", require(ScoreRoute));
 
-app.listen(PORT, "192.168.1.212", () => {
-  console.log(`Serveur démarré sur http://192.168.1.212:${PORT}`);
+app.listen(PORT, IP_ADDRESSE, () => {
+  console.log(`Serveur démarré sur http://${IP_ADDRESSE}:${PORT}`);
 });
